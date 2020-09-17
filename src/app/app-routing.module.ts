e896e0020af63065e1070fc798d97dd5313ec0f2
ngocnt2607@gmail.com <thanhngoc.nguyen@studenti.unicam.it>
2020-09-17 14:26:34 +0700

    1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,49 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {CartComponent} from "./components/cart/cart.component";
+import {CheckoutComponent} from "./components/checkout/checkout.component";
+import {ProductComponent} from "./components/product/product.component";
+import {ThankyouComponent} from "./components/thankyou/thankyou.component";
+import { MyCourseComponent } from './my-course/my-course.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'product/:id', component: ProductComponent
+  },
+  {
+    path: 'cart', component: CartComponent
+  },
+  {
+    path: 'checkout', component: CheckoutComponent
+  },
+  {
+    path: 'thankyou', component: ThankyouComponent
+  },
+  {
+    path: 'mycourse' , component : MyCourseComponent
+  },
+  {
+    path: 'coursedetail', component: CourseDetailComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ src/app/my-course/my-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mg-my-course',
+  templateUrl: './my-course.component.html',
+  styleUrls: ['./my-course.component.scss']
+})
+export class MyCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
